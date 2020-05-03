4ba79d2cf222cc9f79376e283d525ff638496f0d
afos2 <aef___@hotmail.com>
2020-05-03 17:18:13 -0500

    Updated about data

--- src/app/State-Management/Data/about-me-data.ts
+++ src/app/State-Management/Data/about-me-data.ts
@@ -2,15 +2,15 @@ import { AboutData } from 'src/app/Models/AboutData';
 
 export const meData: Array<AboutData> = [
     {
-        header: "Summary",
+        header: "Professional Summary",
         content: "Five years of professional experience in full stack software development utilizing emerging frameworks, developed leadership experience through completion of Digital Technology Leadership Program, and a growing sense of business acumen to enforce building software to meet the needs of client’s requirements."
     },
     {
-        header: "Summary",
-        content: "Five years of professional experience in full stack software development utilizing emerging frameworks, developed leadership experience through completion of Digital Technology Leadership Program, and a growing sense of business acumen to enforce building software to meet the needs of client’s requirements."
+        header: "About this website",
+        content: "If you're wondering why there's so much code on this website; well, that's because I'm showcasing my skills! Check out the code. I use Angular 9 with NGRX. I mimic what would happen in this Angular application if data were to come in from an API service."
     },
     {
-        header: "Summary",
-        content: "Five years of professional experience in full stack software development utilizing emerging frameworks, developed leadership experience through completion of Digital Technology Leadership Program, and a growing sense of business acumen to enforce building software to meet the needs of client’s requirements."
+        header: "Personal Summary",
+        content: "I am married with two dogs. I have a long list of hobbies! My list changes, but currently my list includes woodworking, exercising, fishing, and aquariums."
     }
 ]
